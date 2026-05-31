8a7eba35e7bd9e67bda07183a557f2a989696f79
SRI HARSHA <229003047@sastra.ac.in>
2026-05-31 12:36:45 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,78 @@
+console.log("MY NAME IS HARSHA I AM LEARNING JAVASRIPT");
+let a=10;
+let b=20;
+console.log("ADDITION OF TWO NUMBERS IS",a+b);
+console.log("AREA OF THE RECTANGLE IS:",a*b);
+console.log("AREA OF THE CIRCLE OF RADIUS 5 IS:",3.14*5*5);
+let n = 15;
+if(n%2==0)
+{
+    console.log("even");
+}
+else
+{
+
+    console.log("odd");
+}
+let x=-5
+if(x>0)
+{
+    console.log("positive number");
+}
+else if (x<0)
+{
+    console.log("negative number");
+}
+else
+{
+    console.log("it is a zero");
+}
+let y=40;
+let z= 80;
+if(y>z)
+{
+    console.log(y + "is largest number");
+
+}
+else
+{
+    console.log(x+"is largest number");
+}
+let age=19;
+if(age>=18)
+{
+    console.log("elgible for voting");
+}
+else
+{
+    console.log("not elgible for voting");
+}
+let year = 2024;
+
+if ((year % 4 == 0 && year % 100 !== 0) || year % 400 == 0)
+{
+    console.log("Leap Year");
+} else 
+{
+    console.log("Not a Leap Year");
+}
+let q= 10;
+let w = 20;
+let e = 30;
+
+if (q>= w && q >= e) {
+    console.log(q + " is largest");
+} else if (w>= q && w >= e) {
+    console.log(w + " is largest");
+} else {
+    console.log(e+ " is largest");
+}
+let num=55;
+if(num%5==0 && num %11==0)
+{
+
+    console.log("the number is divisible by both 5 and 11");
+}
+else{
+    console.log("the number is not divisible ny 5 an 11");
+}
